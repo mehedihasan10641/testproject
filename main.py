021c7adc2fe96bd5ea65ae28a42d5db18abcdb51
mehedihasan10641 <mehedihasan10641@gmail.com>
2024-08-30 19:34:51 +0600

    1st Commit

---
+++ main.py
@@ -0,0 +1,10 @@
+print("Hello\nWorld")
+print("Hello\tWorld")
+print('Python is included in CodeSpeedy\r123456')
+print("Hello World." + " I am Rubel")
+print(len("Hello World"))
+a = "Apple"
+b = str(a)
+print(b.upper())
+
+
